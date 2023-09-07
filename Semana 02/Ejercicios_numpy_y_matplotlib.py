dc6184231d133b75fd8683179213f44194e2e56b
KuroSama-TC <TilledCrib746@outlook.es>
2023-09-07 16:06:14 -0500

    Ejercicios de python, falta terminar

---
+++ Semana 02/Ejercicios_numpy_y_matplotlib.py
@@ -0,0 +1,26 @@
+## Introduccion a la libreria NumPy
+# Autor: Reyes Iglesias Bruno Fernando
+## Problema 1
+print('\nProblema 1\n')
+# Importar la biblioteca NumPy
+import numpy as np
+# Generar una matriz de 4x5 con ceros y unos
+matriz_ceros = np.zeros((2, 5), dtype=int)
+matriz_unos = np.ones((2, 5), dtype=int)
+matriz = np.vstack((matriz_ceros, matriz_unos))
+# Imprimir la matriz
+print(matriz)
+## Problema 2
+print('\nProblema 2\n')
+# Importar la biblioteca NumPy
+import numpy as np
+# Generar vector con diferentes numeros
+vector=[32,4,81,np.exp(2.5),np.cos(np.pi/3)]
+print(vector)
+## Problema 3
+print('\nProblema 3\n')
+# Importar la biblioteca NumPy
+import numpy as np
+# Generar vector con diferentes numeros
+vector=[32,4,81,np.exp(2.5),np.cos(np.pi/3)]
+print(vector)
