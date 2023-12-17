dec15bbdbfca493e6f5be4cdd82fe305f143a4a3
Bruno Reyes <65099915+KuroSamaTC@users.noreply.github.com>
2023-12-17 17:36:01 -0500

    Actualizar Practica04.py

--- Semana 06/Practica04.py
+++ Semana 06/Practica04.py
@@ -3,7 +3,6 @@
 
 ## Librerías usadas
 import numpy as np
-import sympy as sp
 from scipy.integrate import ode, odeint
 import matplotlib.pyplot as plt
 
@@ -15,15 +14,19 @@ R = 5  # ft  Radio del tanque
 Ro = 0.5  # ft  Radio del orificio
 V0 = F/CA0  # ft^3/s  Flujo volumétrico
 g = 32  # ft/s^2  Aceleración por gravedad
+
 # 1. A volumen constante de 250 ft^3
 VR = 250  # ft^3  Volumen del reactor en estado estacionario
 tau = VR/V0  # s  Tiempo de residencia
-def modelo1(CA,t):
+def modelo1(CA, t):
     dCAdt = (CA0-CA-k*(CA**2)*tau)/tau
     return dCAdt
 tspan = np.linspace(0,50,101)  # Intervalo de tiempo
 x0 = CA0  # Concentración inicial de A
 sol = odeint(modelo1,x0,tspan)
-print(sol)
-plt.plot(tspan,sol[:,0])
-plt.show()
+
+# 2. A volumen variable
+As = np.pi()*Ro  # Area del orificio
+def modelo2(CA, t):
+    dCAdt = (CA0*(V0/VR))
+
